e74cd4b3044b57bec41cf82b113caf352e2ae8d6
Dorii0513 <bobodorii0513@gmail.com>
2022-03-13 22:50:39 +0900

    스터디 피드백 추가

--- java/src/q9498.java
+++ java/src/q9498.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class q9498 {
     public static void main(String[] args){
         
-        Scanner input = new Scanner(System.in); //scanner 클래스 객체 생성
+        Scanner input = new Scanner(System.in); 
         
-        int score = input.nextInt(); // scanner를 통해 점수(변수) 입력 받기
+        int score = input.nextInt(); 
         
-        String grade; // 문자열 grade 선언
+        String grade;
         
-        switch(score/10){ // score/10에 대한 switch 함수 생성 (앞 자릿수를 구별하여 grade를 다르게 지정)
+        switch(score/10){ 
             case 10 :
             case 9 :
                 grade = "A";
@@ -23,10 +23,12 @@ public class q9498 {
             case 6 :
                 grade = "D";
                 break;
-            default :   // 나머지 case는 모두 grade를 "F"로 함
+            default :  
                 grade = "F";
                 break;
-        }
-    System.out.print(grade); // grade값 출력
+        } 
+        input.close();
+        
+    System.out.print(grade); 
     }
 }
