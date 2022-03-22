b5e0831f488045e7ec2177e96b9dabf0f2b3e96b
Dorii0513 <bobodorii0513@gmail.com>
2022-03-22 21:45:35 +0900

    0322

---
+++ java/src/q1110.java
@@ -0,0 +1,35 @@
+import java.io.*;
+public class q1110 {
+
+    public static void main(String[] args)throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        if(N < 10){
+            N = N * 10;
+        }
+        int num1 = N / 10;
+        int num2 = N % 10;
+        int newNum;
+        int i = 0; 
+        while(true){
+            i++;
+            if(num1 + num2 >= 10){
+                newNum = num2 * 10 + (num1 + num2 - 10);
+                //num1 = num2;
+                //num2 = num1 + num2 - 10;
+            } else{
+                newNum = num2 * 10 + (num1 + num2);
+                //num1 = num2;
+                //num2 = num1 + num2;
+            }
+            num1 = newNum / 10;
+            num2 = newNum % 10;
+            System.out.println("새로운 수: " + newNum);
+            if(N == newNum){break;}
+        }
+        System.out.print(i);
+    }
+}
