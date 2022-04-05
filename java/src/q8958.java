cb8d58340e956d9cff606ef0dd401dc24ae65919
Dorii0513 <bobodorii0513@gmail.com>
2022-04-05 20:45:42 +0900

    0405

--- java/src/q8958.java
+++ java/src/q8958.java
@@ -4,24 +4,24 @@ public class q8958 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int sum = 0;
         int[] fin = new int[N];
 
         for(int i = 0; i < N; i++){
+            int sum = 0;
             System.out.print((i+1) + "번 째: ");
             String str = br.readLine();
             char[] arr = str.toCharArray();
             for(int k = 0; k < arr.length; k++){
                 int cnt = 0;
                 if(arr[k]=='O'){
-                    cnt += 1;
+                    cnt = 1;
+                    sum += cnt;
                     for(int j = k+1; j < arr.length; j++){
-                        if(arr[k] == arr[j]) {cnt += cnt++;}
-                        else { break;}
-                        k++;
+                        if(arr[k] == arr[j]) {cnt++; sum += cnt;}
+                        else { k = j; break;}
+                        k = j;
                     }
                 }
-                sum += cnt;
             }
             fin[i] = sum;
         }
