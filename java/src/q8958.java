56d522a5d1e708bc2e82b9520448fb2d12fe0f5f
Dorii0513 <bobodorii0513@gmail.com>
2022-04-06 22:18:35 +0900

    0306수정

--- java/src/q8958.java
+++ java/src/q8958.java
@@ -4,29 +4,24 @@ public class q8958 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
         int[] fin = new int[N];
 
         for(int i = 0; i < N; i++){
-            int sum = 0;
             System.out.print((i+1) + "번 째: ");
-            String str = br.readLine();
-            char[] arr = str.toCharArray();
-            for(int k = 0; k < arr.length; k++){
-                int cnt = 0;
-                if(arr[k]=='O'){
-                    cnt = 1;
-                    sum += cnt;
-                    for(int j = k+1; j < arr.length; j++){
-                        if(arr[k] == arr[j]) {cnt++; sum += cnt;}
-                        else { k = j; break;}
-                        k = j;
-                    }
-                }
-            }
-            fin[i] = sum;
+            arr[i] = br.readLine();
         }
+
         for(int i = 0; i < N; i++){
-            System.out.println(fin[i]);
+            int cnt = 0;
+            int sum = 0;
+            for(int k = 0; k < N; k++){
+                if(arr[i].charAt(k)=='O'){
+                    cnt++;
+                } else {cnt = 0;}
+                sum += cnt;
+            }
+            System.out.println(sum);
         }
     } 
 }
